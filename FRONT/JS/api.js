1db0c82e3b3a549e913a72f6d8aabce7f822de08
Juano <jumadariaga21@alumnos.utalca.cl>
2024-04-23 20:21:59 -0400

    agregar producto, pero sin imagen aun

--- FRONT/JS/api.js
+++ FRONT/JS/api.js
@@ -12,3 +12,15 @@ async function crearEmpresa(data) {
 
 })
 }
+
+async function crearproducto(data, empresa) {
+    try {
+        const url = `http://localhost:3000/api/empresa/addproducto/${empresa}`;
+        const respuesta = await axios.post(url, data);
+        // Manejar la respuesta aquí
+        console.log(respuesta.data);
+    } catch (error) {
+        // Manejar los errores aquí
+        console.error("Error al crear el producto:", error);
+    }
+}
