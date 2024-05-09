a4bb9cb3e0a7526c33922084422308ba36a78d46
Juano <jumadariaga21@alumnos.utalca.cl>
2024-05-08 20:27:54 -0400

    Lista Para trabajar

---
+++ APIREST/server.js
@@ -0,0 +1,24 @@
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const app = express()
+require("dotenv").config()
+app.use(express.json())
+
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.send("")
+})
+mongoose.connect(process.env.MONGODB_URL).then(async ()=>{
+    console.log("CONEXIÓN ESTABLECIDA CON MONGODB")
+}).catch((error)=>{
+    console.log(error)
+})
+
+app.use("/api/usuario",require("./src/routes/usuario"))
+app.use("/api/empresa",require("./src/routes/empresa"))
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Servidor esta corriendo en http://localhost:${process.env.PORT}`)
+})
---
+++ APIREST/src/models/usuario.js
@@ -0,0 +1,15 @@
+
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const usuarioSchema = new Schema(
+    {
+        nombre : {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        contraseña: {type: String, required: true},
+        rol: {type: String}
+    }
+)
+const usuario = mongoose.model("usuario",usuarioSchema)
+
+module.exports = usuario
