914f1f2aef5b021ef7ee738a4470f8e8a62e2347
Juano <jumadariaga21@alumnos.utalca.cl>
2024-05-08 22:04:33 -0400

    Los Datos se guardan
    se mantienen guardados los datos del perfil con el que se ingresa

--- FRONT/JS/login.js
+++ FRONT/JS/login.js
@@ -1,31 +1,22 @@
 const botonLogin = document.getElementById("btn_login")
-botonLogin.onclick = async (event) => {
+localStorage.clear();
+botonLogin.onclick = async () => {
   event.preventDefault();
-  const email = document.getElementById("login-email").value;
-  const password = document.getElementById("login-password").value;
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
 
-  try {
-    let endpoint = ""; // Inicializa la variable del punto final
-    
-    if (`http://localhost:3000/api/usuario/${email}`) {
-      endpoint = `http://localhost:3000/api/usuario/login/${password}/${email}`;
-    } else {
-      endpoint = `http://localhost:3000/api/empresa/login/${password}/${email}`;
-    }
-
-    // Envía una solicitud GET al servidor para iniciar sesión
-    const response = await axios.get(endpoint);
-
-    console.log(response.data);
-
-    // Si la solicitud es exitosa, muestra un mensaje de éxito
-    redirectToPrincipalPage();
+    try {
+      // Envía una solicitud POST al servidor para iniciar sesión
+      const response = await axios.get(`http://localhost:3000/api/usuario/login/${password}/${email}`);
+      console.log(response.data)
+      localStorage.setItem('usuario', JSON.stringify(response.data));
+      // Si la solicitud es exitosa, muestra un mensaje de éxito
+      redirectToPrincipalPage();
   } catch (error) {
-    // Si hay un error, muestra un mensaje de error
-    alert('Error al iniciar sesión. Por favor, verifica tus credenciales.');
+      // Si hay un error, muestra un mensaje de error
+      alert('Error al iniciar sesión. Por favor, verifica tus credenciales.');
   }
 }
 
 function redirectToPrincipalPage() {
-  window.location.href = "../HTML/index.html";
-}
+  window.location.href = "../HTML/index 2.html";}
