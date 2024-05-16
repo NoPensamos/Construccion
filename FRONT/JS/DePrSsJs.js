2797ca236d7b3a17f056514ffc8cdca824660a40
Juano <jumadariaga21@alumnos.utalca.cl>
2024-05-16 03:15:52 -0400

    Falta Linkeo de pagina

---
+++ FRONT/JS/DePrSsJs.js
@@ -0,0 +1,22 @@
+const inputQuantity = document.querySelector('.input-quantity');
+const btnIncrement = document.querySelector('#increment');
+const btnDecrement = document.querySelector('#decrement');
+
+let valueByDefault = parseInt(inputQuantity.value);
+
+// Funciones Click
+
+btnIncrement.addEventListener('click', () => {
+	valueByDefault += 1;
+	inputQuantity.value = valueByDefault;
+});
+
+btnDecrement.addEventListener('click', () => {
+	if (valueByDefault === 1) {
+		return;
+	}
+	valueByDefault -= 1;
+	inputQuantity.value = valueByDefault;
+});
+
+
